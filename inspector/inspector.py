54263843b2cebe04a4a43b18dd2c84106d379290
Matthew Elwin <elwin@northwestern.edu>
2024-11-04 21:56:41 -0600

    Full interception of the message

--- inspector/inspector.py
+++ inspector/inspector.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
-from rclpy.action import ActionServer
+from rclpy.action import ActionClient, ActionServer
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 from moveit_msgs.action import MoveGroup
 
 class Inspector(Node):
@@ -16,17 +17,30 @@ class Inspector(Node):
 
     def __init__(self):
         super().__init__("inspector")
+        self._cbgroup = MutuallyExclusiveCallbackGroup()
         self._server = ActionServer(self,
                                     MoveGroup,
                                     '/viz/move_action',
-                                    self.move_action_callback)
+                                    self.move_action_callback,
+                                    callback_group = self._cbgroup)
+        self._client = ActionClient(self, MoveGroup, 'move_action')
+        if not self._client.wait_for_server(timeout_sec=10):
+            raise RuntimeException("move_action action server not ready")
 
-    def move_action_callback(self, goal_handle):
+
+    async def move_action_callback(self, goal_handle):
         self.get_logger().info("Move Action Called. Goal Is")
-        self.get_logger().error(f"{goal_handle.request}")
-        self.get_logger().info("END GOAL")
+        self.get_logger().info(f"{goal_handle.request}") # Do more to format this output nicelynicely
+        self.get_logger().info(f"--- End ofrequest dump.----\n")
+        self.get_logger().info("Forwarding the action to the move_group action server")
+        response_goal_handle = await self._client.send_goal_async(goal_handle.request)
+        self.get_logger().info(f"Received response goal handle: Accepted? {response_goal_handle.accepted}")
+        self.get_logger().info("Awaiting the result")
+        response = await response_goal_handle.get_result_async()
+        self.get_logger().info(f"Received the result: {response}") # Do more formatting
         goal_handle.succeed()
-        return MoveGroup.Result()
+        self.get_logger().info("Interception successful, returning the result")
+        return response.result
 
 def main(args=None):
     rclpy.init()
